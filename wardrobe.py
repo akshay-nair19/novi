57dda4c4a483b51a121c3f7407b506d6e1cbacda
ashma2583 <ashtonma2583@gmail.com>
2025-03-27 10:28:11 -0400

    Add files via upload

--- wardrobe.py
+++ wardrobe.py
@@ -1,50 +1,62 @@
-class Shirt:
+class Clothing:
     def __init__(self, color, picture, size):
         self.color = color
         self.picture = picture
         self.size = size
 
 
-class Shirt_list:
-    def __init__ (self, colors: list):
-        self.wardrobe = colors
-    def add_shirt(self, shirt):
-        self.wardrobe.append(shirt)
-    def add_multiple_shirts(self, shirts: list):
-        self.wardrobe.extend(shirts)
-    def remove_shirt(self, shirt):
-        self.wardrobe.remove(shirt)
-    def get_shirts(self):
-        return self.wardrobe
-
-
-class Pant:
+class Shirt(Clothing):
     def __init__(self, color, picture, size):
-        self.color = color
-        self.picture = picture
-        self.size = size
+        super().__init__(color, picture, size)
 
 
-class Pant_list:
-    def __init__ (self, colors: list):
-        self.wardrobe = colors
-    def add_pant(self, pant):
-        self.wardrobe.append(pant)
-    def add_multiple_pants(self, pants: list):
-        self.wardrobe.extend(pants)
-    def remove_pant(self, pant):
-        self.wardrobe.remove(pant)
-    def get_pants(self):
+class Pant(Clothing):
+    def __init__(self, color, picture, size):
+        super().__init__(color, picture, size)
+
+
+class Wardrobe:
+    def __init__(self, items: list):
+        self.wardrobe = items
+
+    def add_item(self, item):
+        self.wardrobe.append(item)
+
+    def add_multiple_items(self, items: list):
+        self.wardrobe.extend(items)
+
+    def remove_item(self, item):
+        self.wardrobe.remove(item)
+
+    def get_items(self):
         return self.wardrobe
+
+
+class Shirt_list(Wardrobe):
+    def __init__(self, shirts: list):
+        super().__init__(shirts)
+
+
+class Pant_list(Wardrobe):
+    def __init__(self, pants: list):
+        super().__init__(pants)
 
 
 class History:
-    def __init__ (self):
+    def __init__(self):
         self.history = []
+
     def wear_outfit(self, outfit: tuple):
         if len(self.history) >= 7:
-            self.history.pop(0)  
+            self.history.pop(0)
         self.history.append(outfit)
+
     def print_history(self):
         for i, (shirt, pant) in enumerate(self.history, 1):
             print(f"Outfit {i}: Shirt({shirt.color}, {shirt.picture}) - Pant({pant.color}, {pant.picture})")
+
+
+
+
+if __name__ == '__main__':
+    print('hello')
