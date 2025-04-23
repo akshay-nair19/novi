7d648fd373976f6054a20ced5a118b92f87baac7
Aditya Rakshit <111544932+DesperateEffort18@users.noreply.github.com>
2025-04-23 18:36:15 -0400

    Add files via upload

--- color_detector.py
+++ color_detector.py
@@ -4,8 +4,8 @@ import numpy as np
 
 color_dict = {
     'yellow': [np.array([25, 100, 100]),np.array([31, 255, 255])],
-    'red': [np.array([0, 95, 0]), np.array([9, 255, 216])],
-    'red': [np.array([170, 100, 70]), np.array([179, 255, 255])],    
+    'red_low': [np.array([0, 95, 0]), np.array([9, 255, 216])],
+    'red_up': [np.array([170, 100, 70]), np.array([179, 255, 255])],    
     'green': [np.array([32, 98, 50]), np.array([84, 255, 255])],
     'turquoise': [np.array([84, 140, 50]), np.array([90, 255, 255])],
     'orange': [np.array([10, 100, 120]), np.array([25, 255, 255])],
@@ -25,7 +25,10 @@ def color_dectector(img: str, color_dict:dict):
     if img is None:
         raise ValueError("Image not found or invalid path provided.")
     
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    blurred_img = cv2.GaussianBlur(img, (11, 11), 0)
+    hsv_img = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2HSV)
+    #hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  old
     detected_colors = []
     for color_name, hsv_range in color_dict.items():
         #print('running')
@@ -34,13 +37,16 @@ def color_dectector(img: str, color_dict:dict):
             lower_hsv, upper_hsv = hsv_range
             mask = cv2.inRange(hsv_img,lower_hsv,upper_hsv)
             
+            kernel = np.ones((5, 5), np.uint8)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
             #print(np.count_nonzero(mask))
-            if np.count_nonzero(mask) > 50:  # Threshold to reduce false positives
+            if np.count_nonzero(mask) > 500:  # Threshold to reduce false positives
                 print(f"Detected color: {color_name}")
                 detected_colors.append(color_name)
         #print(50*'-')
     return detected_colors
         
         
-#print(color_dectector('redandpink2.png', color_dict))
+print(color_dectector('wheel.png', color_dict))
 
